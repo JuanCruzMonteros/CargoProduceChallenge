3c2f0a393d854425cbde5964b8bd4e3ec9995fb9
JuanCruzMonteros <jjuancruzrey@gmail.com>
2023-07-18 23:40:04 -0300

    timer component is added

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -8,10 +8,23 @@ import { ManageTaskService } from './services/manage-task.service';
 import { EditTaskDialogComponent } from './components/edit-task-dialog/edit-task-dialog.component';
 import { ManageTasksComponent } from './pages/manage-tasks/manage-tasks.component';
 import { TasksListComponent } from './containers/tasks-list/tasks-list.component';
+import { CountdownModule } from 'ngx-countdown';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
-  declarations: [ManageTasksComponent, EditTaskDialogComponent, TasksListComponent],
-  imports: [CommonModule, TasksRoutingModule, MaterialModule, SharedModule],
+  declarations: [
+    ManageTasksComponent,
+    EditTaskDialogComponent,
+    TasksListComponent,
+    TimerComponent,
+  ],
+  imports: [
+    CommonModule,
+    TasksRoutingModule,
+    MaterialModule,
+    SharedModule,
+    CountdownModule,
+  ],
   providers: [ManageTaskService],
 })
 export class TasksModule {}
